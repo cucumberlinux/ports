2c79fe6274b9f68100b7751e20b45163385a61c3
Scott Court <Z5T1@Z5T1.com>
2018-07-06 14:29:59 -0400

    Added glibc and upgraded it to 2.27

---
+++ cucumber/base/glibc/doinst.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Installs the configuration file specified by $1 ($1 should be the filename
+# WITHOUT the .new extension). The behavior of this function can be controlled
+# by setting the NEW_FILE_ACTION variable in the environment to one of the
+# following:
+#
+# OVERWRITE: blindly overwrites any existing files.
+# KEEP (DEFAULT): keep the old files in place, leaving the new files as .new.
+# REPLACE: replace any existing files, but save a copy of the old files as .old.
+install_new_file () {
+	# Check if the files are identical, and remove the new one if they are.
+	diff -q $1.new $1 && rm $1.new
+
+	if [ -z "$NEW_FILE_ACTION" -o "$NEW_FILE_ACTION" == "KEEP" ]; then
+		if [ ! -e $1 ]; then
+			mv $1.new $1
+		fi
+	elif [ "$NEW_FILE_ACTION" == "OVERWRITE" ]; then
+		mv $1.new $1
+	elif [ "$NEW_FILE_ACTION" == "REPLACE" ]; then
+		if [ -e $1 ]; then
+			mv $1 $1.old
+		fi
+		mv $1.new $1
+	fi
+}
+
+install_new_file etc/nscd.conf
+install_new_file etc/nsswitch.conf
+install_new_file etc/ld.so.conf
+install_new_file etc/rpc
